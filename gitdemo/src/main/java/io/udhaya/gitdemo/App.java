208f25643996e85bfa71b043c593b653baba971c
Udhayakumar <audhaya@gmail.com>
2022-02-09 16:05:16 +0530

    Git Demo testing

---
+++ gitdemo/src/main/java/io/udhaya/gitdemo/App.java
@@ -0,0 +1,13 @@
+package io.udhaya.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
